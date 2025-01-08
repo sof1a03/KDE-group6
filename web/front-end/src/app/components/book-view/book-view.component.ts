06eddbcec4ef838f804da7aa4d93bd5a097574dc
kasperkoffie <vdpolkasper@gmail.com>
2025-01-08 18:30:45 +0100

    Diversified dummy data

--- web/front-end/src/app/components/book-view/book-view.component.ts
+++ web/front-end/src/app/components/book-view/book-view.component.ts
@@ -14,14 +14,14 @@ import { CommonModule } from '@angular/common';
 export class BookViewComponent {
   dummy_data = [
     ["Flowers for Algernon", "https://media.s-bol.com/DY9K0jOzlggk/wV6q0xg/550x836.jpg" ],
-    ["Flowers for Algernon", "https://media.s-bol.com/DY9K0jOzlggk/wV6q0xg/550x836.jpg" ],
-    ["Flowers for Algernon", "https://media.s-bol.com/DY9K0jOzlggk/wV6q0xg/550x836.jpg" ],
-    ["Flowers for Algernon", "https://media.s-bol.com/DY9K0jOzlggk/wV6q0xg/550x836.jpg" ],
-    ["Flowers for Algernon", "https://media.s-bol.com/DY9K0jOzlggk/wV6q0xg/550x836.jpg" ],
-    ["Flowers for Algernon", "https://media.s-bol.com/DY9K0jOzlggk/wV6q0xg/550x836.jpg" ],
-    ["Flowers for Algernon", "https://media.s-bol.com/DY9K0jOzlggk/wV6q0xg/550x836.jpg" ],
-    ["Flowers for Algernon", "https://media.s-bol.com/DY9K0jOzlggk/wV6q0xg/550x836.jpg" ],
-    ["Flowers for Algernon", "https://media.s-bol.com/DY9K0jOzlggk/wV6q0xg/550x836.jpg" ]
+    ["Lord of the Rings", "https://m.media-amazon.com/images/I/913sMwNHsBL._SL1500_.jpg" ],
+    ["The Perfume", "https://upload.wikimedia.org/wikipedia/en/f/f5/PerfumeSuskind.jpg" ],
+    ["Fall or Dodge; in Hell", "https://m.media-amazon.com/images/I/91Z1I-2LQpL._AC_UF1000,1000_QL80_.jpg" ],
+    ["Brave New World", "https://m.media-amazon.com/images/I/91D4YvdC0dL._AC_UF894,1000_QL80_.jpg" ],
+    ["The Midnight Library", "https://media.s-bol.com/yyE0mpJQW856/o2jMxV3/547x840.jpg" ],
+    ["Cryptonomicon", "https://m.media-amazon.com/images/I/811HmmUbx3L._AC_UF1000,1000_QL80_.jpg" ],
+    ["Hersenschimmen", "https://media.s-bol.com/vZg12A3m36QM/739x1200.jpg" ],
+    ["Tokyo Vice", "https://m.media-amazon.com/images/I/61TWK--pVVL._AC_UF894,1000_QL80_.jpg" ]
   ]
 
 }
